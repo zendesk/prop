b6a6b7f00890386348c5880d9038c3b9143896ca
Craig Little <craiglttl@gmail.com>
2013-06-10 11:45:48 -0700

    Fix mocha deprecation warning

--- test/helper.rb
+++ test/helper.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'test/unit'
 require 'shoulda'
-require 'mocha'
+require 'mocha/setup'
 require 'time'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
