206e54ca38084e572803db101f2aedb36e237e99
Craig Day <cday@zendesk.com>
2018-05-07 15:53:22 -0700

    add a read for cache to top level prop module

--- lib/prop.rb
+++ lib/prop.rb
@@ -8,7 +8,7 @@ module Prop
   # Short hand for accessing Prop::Limiter methods
   class << self
     extend Forwardable
-    def_delegators :"Prop::Limiter", :read, :write, :cache=, :configure, :configurations, :disabled, :before_throttle
+    def_delegators :"Prop::Limiter", :read, :write, :cache, :cache=, :configure, :configurations, :disabled, :before_throttle
     def_delegators :"Prop::Limiter", :throttle, :throttle!, :throttled?, :count, :query, :reset
   end
 end
