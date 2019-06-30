7d8ef4b88bee346bcb0ea19760c7ea4e898125e9
Vanchi Koduvayur <vanchi@zendesk.com>
2019-06-29 20:01:21 -0700

    CR: update assert statement
    Co-Authored-By: Michael Grosser <michael@grosser.it>

--- test/test_changelog.rb
+++ test/test_changelog.rb
@@ -4,7 +4,8 @@ require_relative 'helper'
 describe Prop::VERSION do
   it "should always add a changelog while bumping versions" do
     changes = File.read("#{File.dirname(__FILE__)}/../Changelog.md")
-    assert changes.include?("## #{Prop::VERSION}"), "version #{Prop::VERSION}"\
+    expected = "## #{Prop::VERSION}"
+    assert changes.include?(expected), "#{expected} not found in Changelog.md"
       "not found in Changelog.md, Please update the Changelog file"
   end
 end
